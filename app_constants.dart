1192ee1373db010682116a62f225ba4c13c0350c
MehradH444 <MehhradHeydarian19@gmail.com>
2026-08-31 22:47:45 +0330

    Create app_constants.dart

---
+++ app_constants.dart
@@ -0,0 +1,15 @@
+class AppConstants {
+  // آدرس‌های سرور (برای شبیه‌ساز اندروید از 10.0.2.2 استفاده می‌شود)
+  static const String baseUrl = 'http://10.0.2.2:3000/api';
+  static const String socketUrl = 'http://10.0.2.2:3000';
+
+  // کلیدهای ذخیره‌سازی محلی
+  static const String tokenKey = 'AUTH_TOKEN';
+  static const String userIdKey = 'USER_ID';
+
+  // ثوابت خال‌های بازی حکم
+  static const String suitHearts = 'HEARTS';
+  static const String suitDiamonds = 'DIAMONDS';
+  static const String suitClubs = 'CLUBS';
+  static const String suitSpades = 'SPADES';
+}
